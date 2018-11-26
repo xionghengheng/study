fa93ad3acba4c05bf65f387257797a58c86368ed
wu <xionghengheng@gmail.com>
2018-11-27 00:45:20 +0800

    Update 二分查找.cpp

--- Interview code snippets/二分查找.cpp
+++ Interview code snippets/二分查找.cpp
@@ -1,35 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 //二分查找-循环版本
-int binarySerch(vector<int> arr, int target)
+int binarySerch(const vector<int>& arr, int target)
 {
     int begIdx = 0;
     int endIdx = (int)(arr.size() - 1);
     while (begIdx <= endIdx)
     {
-        int mid = (begIdx + endIdx) / 2;
-        if (target > arr[mid]) begIdx = mid + 1;
-        else if (target < arr[mid]) endIdx = mid - 1;
-        else return mid;
+        int midIdx = (begIdx + endIdx) / 2;
+        if (target > arr[midIdx]) begIdx = midIdx + 1;
+        else if (target < arr[midIdx]) endIdx = midIdx - 1;
+        else return midIdx;
+    }
+    return -1;
+}
+
+int binarySerchRecursion(const vector<int>& arr, int target, int begIdx, int endIdx)
+{
+    int midIdx = (begIdx + endIdx) / 2;
+    if (begIdx > endIdx)
+    {
+        return -1;
+    }
+    if (target > arr[midIdx])
+    {
+        return binarySerchRecursion(arr, target, midIdx + 1, endIdx);
+    }
+    else if (target < arr[midIdx])
+    {
+        return binarySerchRecursion(arr, target, midIdx, endIdx - 1);
+    }
+    else
+    {
+        return midIdx;
     }
     return -1;
 }
 
 //二分查找-递归版本
-int binarySerch2(vector<int> arr, int target)
+int binarySerch2(const vector<int>& arr, int target)
 {
-    return -1;
+    return binarySerchRecursion(arr, target, 0, (int)(arr.size() - 1));
 }
 
 int main()
 {
-    vector<int> arr;
-    arr.push_back(1);
-    arr.push_back(20);
-    arr.push_back(57);
-    arr.push_back(92);
-    arr.push_back(109);
-    arr.push_back(308);
-    arr.push_back(1023);
-    cout << "二分查找结果索引=" << binarySerch(arr, 1) << endl;
-    cout << "二分查找结果索引=" << binarySerch2(arr, 1) << endl;
+    vector<int> arr = {1,13,20,57,92,109,308,1023,4502};
+    std::cout << "二分查找结果索引=" << binarySerch(arr, 308) << std::endl;
+    std::cout << "二分查找结果索引=" << binarySerch(arr, 0) << std::endl;
+    std::cout << "二分递归查找结果索引=" << binarySerch2(arr, 1023) << std::endl;
+    std::cout << "二分递归查找结果索引=" << binarySerch2(arr, 4501) << std::endl;
     return 1;
 }
